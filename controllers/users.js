b54af4fb46b2d897007099e8474ce5951bf71a01
brandon-w1205 <bwu1205@gmail.com>
2022-09-13 14:12:51 -0700

    adds npm setups for index and users

---
+++ controllers/users.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+const crypto = require('crypto-js')
+const bcrypt = require('bcrypt')
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const layout = require('express-ejs-layouts')
+const methodOverride = require('method-override')
 
 const app = express()
 const PORT = 8000
 app.set('view engine', 'ejs')
 app.use(layout)
+
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride("_method"))
 
 app.get('/', (req, res) => {
     res.render('home')
