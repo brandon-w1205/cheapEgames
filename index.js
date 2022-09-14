6122efb547b55401a5640abde1091533dcd1fb64
brandon-w1205 <bwu1205@gmail.com>
2022-09-13 20:58:35 -0700

    adds login functionality

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const layout = require('express-ejs-layouts')
 const methodOverride = require('method-override')
+const cookieParser = require('cookie-parser')
+const db = require('./models')
+require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT ||  8000
@@ -8,11 +11,40 @@ app.set('view engine', 'ejs')
 app.use(layout)
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride("_method"))
+app.use(cookieParser())
 
+// our custom auth middleware
+app.use(async (req, res, next) => {
+    console.log('hello from a middleware')
+    res.locals.myData = 'hello, fellow route'
+    // if there is a cookie on the incoming request
+    if (req.cookies.userId) {
+        // decrypt the user id before we look up the user in the db
+        const decryptedId = crypto.AES.decrypt(req.cookies.userId.toString(), process.env.ENC_SECRET) 
+        const decryptedIdString = decryptedId.toString(crypto.enc.Utf8)
+        // look up the user in the db
+        const user = await db.user.findByPk(decryptedIdString)
+        // mount the user on the res.locals
+        res.locals.user = user
+    } else {
+        // if there is no cookie -- set the user to be null in the res.locals
+        res.locals.user = null
+    }
+    // move on to the next route or middleware in the chain
+    next()
+})
+
+
+// route definitions
 app.get('/', (req, res) => {
+    console.log('the currently logged in user is:', res.locals.user)
     res.render('home')
 })
 
+// Controllers
+app.use('/users', require('./controllers/users'))
+
+// listen on a port
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
+    console.log(`listening on port: ${PORT}`)
 })
