53269d51c781faf13273c0a4456ee7476375bdf2
brandon-w1205 <bwu1205@gmail.com>
2022-09-16 11:50:10 -0700

    adds rawg info to specific game information page

--- controllers/results.js
+++ controllers/results.js
@@ -24,14 +24,20 @@ router.get(`/:id`, async (req, res) => {
         console.log(req.query.id)
         const gameUrl = `https://www.cheapshark.com/api/1.0/games?title=${req.params.id}&limit=1`
         const response = await axios.get(gameUrl)
-            // .then(response => {
-                console.log(response.data)
-                if(response.data.length === 0) {
-                    res.render('404')
-                } else {
-                    res.render('results/game', {gameData: response.data})
-                }
-            // })
+        const gameRawgUrl = `https://api.rawg.io/api/games/${req.query.id}?key=${process.env.RAWG_Key}`
+        const responseRawg = await axios.get(gameRawgUrl)
+
+
+        res.render('results/game', {
+            gameDeal: response.data,
+            gameInfo: responseRawg.data
+        })
+            // console.log(response.data)
+            // if(response.data.length === 0) {
+            //     res.render('404')
+            // } else {
+            //     res.render('results/game', {gameData: response.data})
+            // }
     } catch(err) {
         console.log(err)
         res.render('404')
