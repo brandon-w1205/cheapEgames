f89e5cb1cbbce55a27e03cfb4750afb763415697
brandon-w1205 <bwu1205@gmail.com>
2022-09-16 10:16:41 -0700

    fixes undefined issue for second page

--- controllers/results.js
+++ controllers/results.js
@@ -20,20 +20,15 @@ router.get('/', async (req, res) => {
 
 router.get(`/:id`, async (req, res) => {
     try {
-        console.log(req.query.name)
-    const gameUrl = `https://www.cheapshark.com/api/1.0/games?title=${req.body.name}&limit=1`
-    const response = await axios.get(gameUrl)
+        console.log(req.params.id)
+        const gameUrl = `https://www.cheapshark.com/api/1.0/games?title=${req.params.id}&limit=1`
+        const response = await axios.get(gameUrl)
         console.log(response.data)
         res.render('results/game', {gameData: response.data})
     } catch(err) {
         console.log(err)
         res.render('404')
     }
-    // if(response.data === []) {
-        
-    // } else {
-        
-    // }
 })
 
 module.exports = router
